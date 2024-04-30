21899a019ccb2e00d21a94e92cb80189ecb0a1e0
Sai <90199989+djsaikrishna@users.noreply.github.com>
2024-04-30 22:20:03 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,17 +1,17 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
+    API_ID = int(env.get("TELEGRAM_API_ID", 29272471))
+    API_HASH = env.get("TELEGRAM_API_HASH", "59e13c9adc5770397c95e4eb96da7571")
+    OWNER_ID = int(env.get("OWNER_ID", 7184153122))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "itsmrunalthakur_bot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "7039231523:AAE8U0rd4SxYzwPjB0X6TxXseq9G-5JsI1M")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002083855725))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "https://mrunalthakur.onrender.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
